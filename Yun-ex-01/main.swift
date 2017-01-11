8273a45e667907b0a65e777d7035f24056e91af8
윤용섭 <yun-yongseob@yun-yongseob-ui-macbook-pro.local>
2017-01-11 18:50:42 +0900

    aa

--- Yun-ex-01/main.swift
+++ Yun-ex-01/main.swift
@@ -8,5 +8,32 @@
 
 import Foundation
 
-print("Hello, World!")
+var num1: Int = 123
+var str1: String = "asfd"
+print(num1, str1)
 
+//선언과 동시에 변수 타입 값 설정하면 매우 깔금
+
+var age: Int = 30
+print(age)
+
+
+var a,b,c,d,e : Int
+a=1
+b=2
+c=3
+d=4
+
+print(a,b,c,d)
+e=5
+print(e)
+var f=a
+print(f)
+
+let pi: Double = 3.14
+var r: Double  = 3
+var wd:Double = pi*r
+print(wd)
+
+print(Int.max)
+print(Int.min)
